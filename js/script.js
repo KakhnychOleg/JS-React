4aafefd2782064983a521eccb1aa57fe9819c982
Kakhnych Oleg <kakhnych.oleg@gmail.com>
2023-11-01 11:15:02 +0200

    complite task film

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
 'use strict';
 
+// Task create window film question **************************************** 
 
-const numberOfFilms = prompt('Сколько фильмов вы уже просмотрели?', '');
+// const numberOfFilms = prompt('Сколько фильмов вы уже просмотрели?', '');
 
-const personalMovieDB = {
-   count: numberOfFilms,
-   movies: {},
-   actors: {},
-   genres: [],
-   privat: false
-};
+// const personalMovieDB = {
+//    count: numberOfFilms,
+//    movies: {},
+//    actors: {},
+//    genres: [],
+//    privat: false
+// };
 
-const a = prompt('Oдин из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Oдин из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Oдин из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Oдин из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// Task END ****************************************************************
 
 
 // console.log(2 + 2 * 2 !== 6);
