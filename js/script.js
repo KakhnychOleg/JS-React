3419c8b94fe89ce7e2c36f759dc348549494a27f
Kakhnych Oleg <kakhnych.oleg@gmail.com>
2023-11-02 16:20:20 +0200

    test && and ||

--- js/script.js
+++ js/script.js
@@ -1,31 +1,87 @@
 'use strict';
 
-// || ***************************************************************
 
-const hamburger = 2;
-const fries = 0;
+// homework - && and ||
+
+let hamburger;
+const fries = NaN;
 const cola = 0;
 const nuggets = 2;
 
-// cola === 1 (можно писать как cola) тоже самое
-if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-   console.log(' Все довольны!');
-} else {
-   console.log('Уходим');
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
 }
+// false
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!')
+// }
+// true
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+// true
+
+
+
+console.log( NaN || 2 || undefined ); // 2
+
+console.log( NaN && 2 && undefined ); //NaN
+
+console.log( 1 && 2 && 3 ); //3
+
+console.log( !1 && 2 || !3 );  //false
+
+console.log( 25 || null && !3 ); //25
+
+console.log( NaN || null || !3 || undefined || 5); // 5
+
+console.log( NaN || null && !3 && undefined || 5); //5
+
+console.log( 5 === 5 && 3 > 1 || 5); //true
+
+
+// || ***************************************************************
+
+// const hamburger = 2;
+// const fries = 0;
+// const cola = 0;
+// const nuggets = 2;
+
+// cola === 1 (можно писать как cola) тоже самое
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+//    console.log(' Все довольны!');
+// } else {
+//    console.log('Уходим');
+// }
 
 // && ****************************************************************
 
-const hamburger = 2;
-const fries = 1;
-const cola = 1;
+// const hamburger = 2;
+// const fries = 1;
+// const cola = 1;
 
 // cola === 1 (можно писать как cola) тоже самое
-if (hamburger === 3 && cola && fries) {
-   console.log(' Я сыт!');
-} else {
-   console.log('Уходим');
-}
+// if (hamburger === 3 && cola && fries) {
+//    console.log(' Я сыт!');
+// } else {
+//    console.log('Уходим');
+// }
 
 
 // Условия If *****************************************************************
@@ -60,7 +116,7 @@ switch (num) {
 // END *********************************************************************
 
 
-Task create window film question **************************************** 
+// Task create window film question **************************************** 
 
 const numberOfFilms = prompt('Сколько фильмов вы уже просмотрели?', '');
 
