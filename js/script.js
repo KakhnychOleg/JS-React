e0a5f7f19d4ae05544414efb92ed385566a00ecc
Kakhnych Oleg <kakhnych.oleg@gmail.com>
2023-11-04 14:14:06 +0200

    for in for

--- js/script.js
+++ js/script.js
@@ -1,16 +1,52 @@
 'use strict';
 
 
-let num = 50;
 
-while (num < 55) {
-   console.log(num);
-   num++;
-} 
+// for in for Вложенные циклы ****************************************************
 
+for (let i = 0; i < 3; i++) {
+   console.log(i);
 
+   for (let j = 0; j < 3; j++) {
+      console.log(j);
+   }
+}
 
+let result = '';
+let lenght = 10;
 
+for (let i = 1; i < lenght; i++) {
+
+   for (let j = 1; j < i; j++) {
+      result += '*';
+   }
+
+   result += '\n';
+}
+
+console.log(result);
+
+// for & while ************************************************************88
+
+// let num = 50;
+
+// while (num < 55) {
+//    console.log(num);
+//    num++;
+// } 
+// do {
+//    console.log(num);
+//    num++;
+// } 
+// while (num < 55);
+
+for (let i = 1; i < 10; i++) {
+   if (i === 6) {
+      continue;
+   }
+
+   console.log(i);
+}
 
 
 // homework - && and || *************************************************************
