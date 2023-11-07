3539834902866ff25c1271c52fa387e7fa5e6cfd
Kakhnych Oleg <kakhnych.oleg@gmail.com>
2023-11-07 14:51:04 +0200

    task function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,42 @@
 'use strict';
 
+// Место для первой задачи
+function sayHello(name) {
+   return 'Привет, ' + name + '!';
+}
+sayHello('Антон');
+console.log(sayHello('Антон'));
 
+// Место для второй задачи
+function returnNeighboringNumbers(number) {
+   return [number--, number, number++];
+}
+returnNeighboringNumbers(5);
+console.log(returnNeighboringNumbers(5));
 
+// Место для третьей задачи
+function getMathResult(num, times) {
+   if (typeof(times) !== 'number' || times <= 0) {
+       return num;
+   }
+
+   let str = '';
+
+   for (let i = 1; i <= times; i++) {
+       if (i === times) {
+           str += `${num * i}`;
+           // Тут без черточек в конце
+       } else {
+           str += `${num * i}---`;
+           // Это тоже самое, что и
+           // str = str + num * i + "---"
+       }
+   }
+
+   return str;
+}
+
+getMathResult(10, 5);
 
 
 // Конвертация валют
