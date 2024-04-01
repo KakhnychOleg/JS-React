35dc1bf0bf3ea24eff02ac28d4bc81e54518a33f
kahnych.o <kahnych.o@hexide-digital.com>
2024-04-01 20:41:46 +0200

    added new film

--- js/script.js
+++ js/script.js
@@ -2,6 +2,22 @@
 
 // alert('Hello');
 
-// const result = prompt("Are you here?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
 
-console.log(typeof(result));
+const peronalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+}
+
+const film1 = prompt("Один из последних просмотренных фильмов?", ""),
+      film2 = prompt("На сколько вы оцениваете его?", ""),
+      film3 = prompt("Один из последних просмотренных фильмов?", ""),
+      film4 = prompt("На сколько вы оцениваете его?", "");
+
+peronalMovieDB.movies[film1] = film2;
+peronalMovieDB.movies[film3] = film4;
+
+console.log(peronalMovieDB);
